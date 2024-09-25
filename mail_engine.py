61e9828bd969022f7e7ccf7bc1f306843b491c88
Jabba <87302129+jabb4@users.noreply.github.com>
2024-09-25 18:32:02 +0200

    Huge overhaul, now a Mail class with send_mail method. Using email.mime instead of email.message

--- mail_engine.py
+++ mail_engine.py
@@ -1,55 +1,64 @@
 import smtplib
-from email import message
-import gnupg
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 
-def encrypt_message(msg):
-    gpg = gnupg.GPG()
+class Mail():
+    """
+    This class is used to send emails using the SMTP protocol.
 
-    ### points to recivers public key ###
-    with open("reciverPublicKey", "r") as rPubKey:
-        r_public_key = rPubKey.read()
+    args:
+        smtp_address (str): The address of the SMTP server. (e.g. "smtp.office365.com" or "192.168.1.1")
+        smtp_port (int): The port of the SMTP server. (e.g. 25)
+        sender (str): The email address of the sender. (e.g. "sender@example.com")
+        password (str): The password of the sender. (e.g. "password123")
 
-    r_public_key = gpg.import_keys(r_public_key)
+    methods:
+        send_mail: This method sends an email to the receiver with the subject and body.
+    """
 
-    if r_public_key.count == 0:
-        return 0
-    else:
-        key_fingerprint = r_public_key.fingerprints[0]
+    def __init__(self, smtp_address, smtp_port, sender, password):
 
-    return str(gpg.encrypt(msg, key_fingerprint, always_trust=True))
+        self.smtp_address = smtp_address
+        self.smtp_port = smtp_port
+        self.sender = sender
+        self.password = password
 
+    def send_mail(self, receiver, subject, body, bcc = [], html = False):
+        """
+        This method sends an email to the receiver with the subject and body.
 
-def send_mail(mail_reciver, mail_subject, mail_message):
-    ### What smtp server your mail is using ###
-    ### Outlook has smtp.office365.com ###
-    smtp_server = "smtp.office365.com"
+        args:
+            receiver (str): The email address of the receiver. (e.g. "reciver@example.com")
+            subject (str): The subject of the email. 
+            body (str): The body of the email.
+            bcc (list): A list of email addresses to BCC. (e.g. ["1@example.com", "2@example.com"])
+            html (bool): Set to True if the body is in HTML format.
+        
+        returns:
+            tuple: A tuple containing the status if the email was sent successfully. The first element is 1 if the email was sent successfully, 0 otherwise. The second element is a message indicating the status of the email.
+        """
 
-    ### What port your smtp server uses ###
-    ### Outlook has 587 ###
-    smtp_port = 587
+        message = MIMEMultipart()
+        message["From"] = self.sender
+        message["To"] = receiver
+        message["Subject"] = subject
+        if bcc:
+            message["Bcc"] = bcc
 
-    ### What mail you are sending from (login credentials) ###
-    mail_sender = "mail@outlook.com"
+        ## Attach the body to the email
+        message.attach(MIMEText(body, "plain"))
+        if html:
+            message.attach(MIMEText(body, "html"))
 
-    ### What password that mail has (login credentials) ###
-    mail_sender_password = "password"
-
-    mail = message.Message()
-    mail.add_header("from", mail_sender)
-    mail.add_header("to", mail_reciver)
-    mail.add_header("subject", mail_subject)
-    mail.set_payload(mail_message)
-
-    try:
-        server = smtplib.SMTP(smtp_server, smtp_port)
-        server.ehlo()
-        server.starttls()
-        server.ehlo()
-
-        server.login(mail_sender, mail_sender_password)
-        server.sendmail(mail_sender, mail_reciver, mail.as_string())
-
-        return "Success: Message was sent!"
-    except:
-        return "ERROR: Message wasn't able to be sent!"
+        ## Create a secure connection with the server and send the email
+        try:
+            with smtplib.SMTP(self.smtp_address, self.smtp_port) as server:
+                server.ehlo()
+                server.starttls()
+                server.ehlo()
+                server.login(self.sender, self.password)
+                server.sendmail(self.sender, receiver, message.as_string())
+            return (1,"Success: Message was sent!")
+        except Exception as e:
+            return (0, f"ERROR: Message was not able to be sent! {type(e).__name__}: {e}")
